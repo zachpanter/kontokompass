8571f96ad89ef190ae26c0d3a9c7bae73f26ad98
Zach Panter <zachpanter@gmail.com>
2024-02-09 02:20:17 -0700

    API ideas added

--- internal/api/api.go
+++ internal/api/api.go
@@ -1,65 +1,12 @@
 package api
 
-import (
-	"github.com/emicklei/go-restful"
-	"net/http"
-)
+// TODO: GET transactions (with filtering options (date range, account, category)
 
-type User struct {
-	Id, Name string
-}
+// TODO: categories
+// GET: Lists available budgeting categories.
+// POST: Creates a new custom category.
 
-func New() *restful.WebService {
-	service := new(restful.WebService)
-	service.
-		Path("/users").
-		Consumes(restful.MIME_XML, restful.MIME_JSON).
-		Produces(restful.MIME_XML, restful.MIME_JSON)
-
-	service.Route(service.GET("/{user-id}").
-		To(FindUser))
-
-	service.Route(service.POST("").
-		To(UpdateUser))
-
-	service.Route(service.PUT("/{user-id}").
-		To(CreateUser))
-
-	service.Route(service.DELETE("/{user-id}").
-		To(RemoveUser))
-
-	return service
-}
-
-func FindUser(request *restful.Request, response *restful.Response) {
-	id := request.PathParameter("user-id")
-	// here you would fetch user from some persistence system
-	usr := &User{Id: id, Name: "John Doe"}
-	response.WriteEntity(usr)
-}
-
-func UpdateUser(request *restful.Request, response *restful.Response) {
-	usr := new(User)
-	err := request.ReadEntity(&usr)
-	// here you would update the user with some persistence system
-	if err == nil {
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, err)
-	}
-}
-
-func CreateUser(request *restful.Request, response *restful.Response) {
-	usr := User{Id: request.PathParameter("user-id")}
-	err := request.ReadEntity(&usr)
-	// here you would create the user with some persistence system
-	if err == nil {
-		response.WriteEntity(usr)
-	} else {
-		response.WriteError(http.StatusInternalServerError, err)
-	}
-}
-
-func RemoveUser(request *restful.Request, response *restful.Response) {
-	// here you would delete the user from some persistence system
-}
+// TODO: /categories/{category_id}:
+// GET: Gets details of a specific category.
+// PUT: Edits a category.
+// DELETE: Deletes a category.
