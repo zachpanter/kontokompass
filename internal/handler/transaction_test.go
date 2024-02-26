ec2314a2ad1f45879f885f1e89de7266b333e12b
Zach Panter <zachpanter@gmail.com>
2024-02-26 13:47:32 -0700

    POST transaction unit test implemented

--- internal/handler/transaction_test.go
+++ internal/handler/transaction_test.go
@@ -1,8 +1,10 @@
 package handler_test
 
 import (
+	"bytes"
 	"context"
 	"database/sql"
+	"encoding/json"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/assert/v2"
 	"github.com/zachpanter/kontokompass/internal/config"
@@ -32,7 +34,7 @@ func (mds MockDB) TransactionInsert(ctx context.Context, arg storage.Transaction
 	return nil
 }
 
-func TestMyGinEndpoint(t *testing.T) {
+func SetupTesting() *handler.API {
 	// SETUP
 	ctx := context.Background()
 	conf := config.NewConfig()
@@ -43,6 +45,11 @@ func TestMyGinEndpoint(t *testing.T) {
 
 	// Set Gin to Test mode to disable logging
 	gin.SetMode(gin.TestMode)
+	return newHandler
+}
+
+func TestTransactionSelect(t *testing.T) {
+	newHandler := SetupTesting()
 
 	// Create a test request
 	req, err := http.NewRequest("GET", "/transaction/123", nil)
@@ -56,6 +63,42 @@ func TestMyGinEndpoint(t *testing.T) {
 	// Serve the request through the router
 	newHandler.Router.ServeHTTP(rr, req)
 
+	// ASSERTS
+	assert.Equal(t, http.StatusOK, rr.Code)
+	assert.Equal(t, "application/json; charset=utf-8", rr.Header().Get("Content-Type"))
+}
+
+func TestTransactionInsert(t *testing.T) {
+	newHandler := SetupTesting()
+
+	//payload := map[string]interface{}{
+	//	"name":  "John Doe",
+	//	"email": "john.doe@example.com",
+	//}
+
+	var payload storage.TransactionInsertParams
+	payload.TaClassificationText = "foo_class"
+	payload.TaDescription = "foo_desc"
+	payload.TaCredit.Float64 = 0.0
+	payload.TaDebit.Float64 = 0.0
+	payload.TaBalance = 0.0
+	payload.TaPostdate = time.Now()
+
+	payloadBytes, _ := json.Marshal(payload)
+
+	// Create a test request
+	req, err := http.NewRequest("POST", "/transaction", bytes.NewBuffer(payloadBytes))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a ResponseRecorder
+	rr := httptest.NewRecorder()
+
+	// Serve the request through the router
+	newHandler.Router.ServeHTTP(rr, req)
+
+	// ASSERTS
 	assert.Equal(t, http.StatusOK, rr.Code)
 	assert.Equal(t, "application/json; charset=utf-8", rr.Header().Get("Content-Type"))
 }
